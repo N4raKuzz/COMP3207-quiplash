ad2d7055ac51df6b8e3d9bbad9f4b7382a1fe6b3
N4raKuzz <zzynrk@gmail.com>
2023-11-05 19:27:05 +0800

    add player register/login

--- player_login/__init__.py
+++ player_login/__init__.py
@@ -2,7 +2,7 @@ import logging
 import json
 from azure.functions import HttpRequest, HttpResponse
 from azure.cosmos import CosmosClient
-from shared_code.pwd_encrypt_md5 import md5
+from shared_code.str_utils import StringUtils
 
 URL = "https://cosmos4quiplash.documents.azure.com:443/"
 KEY = "MAS055fkgHmtlQcH0hxTZQ5FIkwe0B33mCV8sTLikSlxoT9payvASatqtAupocgz62N5xv0Q6CTkACDbKkfUeQ=="
@@ -10,8 +10,6 @@ DATABASE_NAME = "quiplashDB"
 CONTAINER_NAME = "player"
 
 def main(req: HttpRequest) -> HttpResponse:
-    logging.info('Processing Player_Login request.')
-
     logging.info('Start player_login function')
 
     client = CosmosClient(URL, credential=KEY)
@@ -24,15 +22,15 @@ def main(req: HttpRequest) -> HttpResponse:
     username = register_json["account"]
     password = register_json["password"]
 
-    user = list(container.query_items(query=f"SELECT * FROM c WHERE c.username = '{username}' ", enable_cross_partition_query=True))
+    user = list(player_container.query_items(query=f"SELECT * FROM c WHERE c.username = '{username}' "))
 
     result = False
     msg = ""
-    if user["password"] == md5(password):
+    if user["password"] == StringUtils.md5(password):
         result = True
-        msg = "Cridential Verified"
+        msg = "OK"
     else:       
         msg = "Username or password incorrect"
 
 
-    return func.HttpResponse(json.dumps({"result": result, "msg": msg }))
+    return HttpResponse(json.dumps({"result": result, "msg": msg }))
--- shared_code/pwd_encrypt_md5.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def md5(password):
-
-    hash_object = hashlib.md5()
-    hash_object.update(password.encode('utf-8'))
-    hashed_password = hash_object.hexdigest()
-    
-    return hashed_password
