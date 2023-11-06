f92d10b1c6ab0063d4577f251c0f71f4fc3f98b9
N4raKuzz <zzynrk@gmail.com>
2023-11-07 01:14:07 +0800

    add translator; add get prompt

--- get/__init__.py
+++ get/__init__.py
@@ -1,24 +1,45 @@
 import logging
-
+import json
 from azure.functions import HttpRequest, HttpResponse
+from azure.cosmos import CosmosClient
 
+URL = "https://cosmos4quiplash.documents.azure.com:443/"
+KEY = "MAS055fkgHmtlQcH0hxTZQ5FIkwe0B33mCV8sTLikSlxoT9payvASatqtAupocgz62N5xv0Q6CTkACDbKkfUeQ=="
+DATABASE_NAME = "quiplashDB"
+CONTAINER_NAME = "prompt"
 
 def main(req: HttpRequest) -> HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Start prompt_get function')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    client = CosmosClient(URL, credential=KEY)
+    database = client.get_database_client(DATABASE_NAME)
+    container = database.get_container_client(CONTAINER_NAME)
+    
+    input = req.get_json()  # {"players":  [list of usernames], "language": "langcode"}
+    players = input["players"]
+    langcode = input["language"]
 
-    if name:
-        return HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+    query = f"""
+    SELECT c
+    FROM c 
+    WHERE c.username IN ({','.join([f"'{player}'" for player in players])}) AND ARRAY_LENGTH(
+        ARRAY(SELECT VALUE t FROM t IN c.texts WHERE t.language = '{langcode}')
+    ) > 0
+    """
+
+    # Execute the SQL query
+    prompts = container.query_items(query=query, enable_cross_partition_query=True)
+
+    output = []
+    for p in prompts:
+        texts = p["texts"]
+        for text in texts:
+            if text['language'] == langcode:
+                new_item = {
+                    "id": texts["id"],
+                    "text": text['text'],
+                    "username": texts["username"]
+                }
+                output.append(new_item)
+    
+    return HttpResponse(json.dumps(output))
---
+++ shared_code/translator.py
@@ -0,0 +1,74 @@
+
+import requests
+import uuid
+import json
+
+key = "1b6c7be719824f4998acfa440cb5f77e"
+endpoint = "https://api.cognitive.microsofttranslator.com/"
+location = "uksouth"
+
+
+supported_languages = set(["en", "es", "it", "sv", "ru", "id", "bg", "zh-Hans"])
+
+def detect(text):
+    
+    url = endpoint + '/detect'
+    params = {
+        'api-version': '3.0'
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    if "language" in response[0] and "score" in response[0]:
+        language = response[0]['language']
+        score = response[0]['score']
+
+        return language,score
+    else:
+        return None, None
+
+def translate(text, languages):
+
+    url = endpoint + '/translate'
+
+    params = {
+        'api-version': '3.0',
+        'to': languages
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Extract and format the translations
+    result = []
+    for t in response[0]['translations']:
+        result.append({
+            "language": t['to'],
+            "text": t['text']
+        })
+
+    return result
+
